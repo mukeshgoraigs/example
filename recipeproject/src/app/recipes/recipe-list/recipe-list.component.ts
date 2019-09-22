bbffcf09dfea9dff424864e48055eea2768a1358
mukeshgoraigs <mukeshgorai24@gmail.com>
2019-09-23 00:05:12 +0530

    add new files

---
+++ recipeproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]=[
+    new Recipe('A test Recipe','This is simply a test',
+    'https://asset.slimmingworld.co.uk/content/media/11596/jackfruit-chilli-iceland_sw_recipe.jpg?v1=JGXiore20qg9NNIj0tmc3TKfKw-jr0s127JqqpCA2x7sMviNgcAYh1epuS_Lqxebn9V_qusKHfwbF7MOUrAPptzBhXIUL1Xnq2Mmdvx4fOk&width=552&height=552'),
+    new Recipe('A test Recipe','This is simply a test',
+    'https://asset.slimmingworld.co.uk/content/media/11596/jackfruit-chilli-iceland_sw_recipe.jpg?v1=JGXiore20qg9NNIj0tmc3TKfKw-jr0s127JqqpCA2x7sMviNgcAYh1epuS_Lqxebn9V_qusKHfwbF7MOUrAPptzBhXIUL1Xnq2Mmdvx4fOk&width=552&height=552')
+ 
+ 
+  ]; 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
